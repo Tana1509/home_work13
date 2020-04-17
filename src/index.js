bb08cfe3d1d89157df4e15578aec6aee211244f1
Tana1509 <tana1509@gmail.com>
2020-04-17 14:06:17 +0300

    new sectoin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-jQuery(document).ready(function($) {});
+jQuery(document).ready(function($) {
+    // nav
+    let menuBtn = $(".header__burger");
+    console.log(menuBtn);
+    menuBtn.on("click", function () {
+        let navigation = $(".header__nav__list");
+        // console.log(navigation);
+        navigation.toggleClass("active");
+    });
+});
